3aa9494d69f782544d112e012be5f8135b703acb
Jdhsiwhvevyudj <140831084+Jdhsiwhvevyudj@users.noreply.github.com>
2023-09-08 17:25:18 +0330

    1725

--- ryan_blog/settings.py
+++ ryan_blog/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'ryan_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgfp_db',
+        'NAME': 'firstkoe_db',
         'USER': 'nimaoghyanosi5@gmail.com', 
-        'PASSWORD': 'hveer6pol9id2cd', 
+        'PASSWORD': '12m92lbd1a2hxiz', 
         'HOST': 'post-tcg-service'
     }
 }
