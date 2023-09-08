1290bbb2500cc89452b651e44f675b464b1a184a
Jdhsiwhvevyudj <140831084+Jdhsiwhvevyudj@users.noreply.github.com>
2023-09-08 18:17:41 +0330

    Update settings.py

--- ryan_blog/settings.py
+++ ryan_blog/settings.py
@@ -84,12 +84,10 @@ WSGI_APPLICATION = 'ryan_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        
-
         'NAME': 'firstkoe_db',
         'USER': 'postgres', 
         'PASSWORD': '12m92lbd1a2hxiz', 
-        'HOST': 'post-tcg-service'
+        'HOST': 'remote.runflare.com:30789'
 
     }
 }
