8494368dd9dfa229fd2ad2d765cd3844d58b00c6
Jdhsiwhvevyudj <140831084+Jdhsiwhvevyudj@users.noreply.github.com>
2023-09-10 01:42:21 +0330

    ,m,m

--- ryan_blog/settings.py
+++ ryan_blog/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ryan_blog/static')
 ]
 
-# Default primary key field type
+# Default primary key field type,
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
