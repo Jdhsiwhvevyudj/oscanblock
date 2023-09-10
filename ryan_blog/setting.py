22e4c32a2da9038c1e45475af9f0bf4fef20370f
Jdhsiwhvevyudj <140831084+Jdhsiwhvevyudj@users.noreply.github.com>
2023-09-10 20:48:46 +0330

    666

--- ryan_blog/setting.py
+++ ryan_blog/setting.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=*km(2btqmy53me8)8b@0i!^@d%uzt-nk$(bmd5kb5uae*u!b$'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!/
 DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
