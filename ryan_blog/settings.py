bf7944a76e0dfef0e8ac8f4753941c801783d5eb
Jdhsiwhvevyudj <140831084+Jdhsiwhvevyudj@users.noreply.github.com>
2023-09-14 15:06:12 +0330

    saas

--- ryan_blog/settings.py
+++ ryan_blog/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/public/static/'
 #STATIC_URL = 'http://188.40.16.3:31434/data/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "public","static")
 STATICFILES_DIRS = [
